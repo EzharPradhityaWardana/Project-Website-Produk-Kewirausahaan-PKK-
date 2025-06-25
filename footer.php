f4a9bc2a6c600860090c79d53824bada6875846b
EzharPradhityaWardana <zharprdhtyaa@gmail.com>
2025-06-25 23:11:36 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,45 @@
+<footer class="footer">
+    <div class="footer-container">
+        <!-- Brand & Social Media -->
+        <div class="footer-brand">
+            <a href="#" class="logo">
+                <img src="imgpage/logo2.png" alt="LalaShoes Logo">
+            </a>
+            <ul class="social-list">
+                <li><a href="https://www.instagram.com/lalashoes_sby?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" class="social-link"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                <li><a href="#" class="social-link"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                <li><a href="wa.me/6289512298925" class="social-link"><ion-icon name="logo-whatsapp"></ion-icon></a></li>
+            </ul>
+        </div>
+
+        <!-- Contact Info -->
+        <div class="footer-section">
+            <h3>Hubungi kami</h3>
+            <p><ion-icon name="location"></ion-icon> Girilaya VIII No.16, Banyu Urip, Kec. Sawahan, Surabaya, Jawa Timur 60253</p> <br>
+            <p><ion-icon name="call"></ion-icon> <a href="tel:+6287853666108">+6287853666108</a></p> <br>
+            <p><ion-icon name="mail"></ion-icon> <a href="mailto:footcap@help.com">lalashoes212@gmail.com</a></p>
+        </div>
+
+        <!-- Jam Operasional -->
+        <div class="footer-section">
+            <h3>Jam Operasional</h3>
+            <p><ion-icon name="time-outline"></ion-icon> Senin - Sabtu: 08.00 - 21.00</p>
+            <p><ion-icon name="time-outline"></ion-icon> Minggu: Tutup</p>
+        </div>
+
+        <!-- Lokasi -->
+        <div class="footer-section">
+            <h3>Lokasi Kita</h3>
+            <iframe 
+                class="footer-map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.6783953395407!2d112.72151837404112!3d-7.277386971513748!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fbfa7d5524b7%3A0x7b33417ba6aa8413!2sToko%20Sepatu%20%22%20Lalashoes%20%22!5e0!3m2!1sid!2sid!4v1738632152541!5m2!1sid!2sid" 
+                width="100%" height="150" style="border:0;" allowfullscreen="" loading="lazy">
+            </iframe>
+        </div>
+    </div>
+
+    <!-- Footer Bottom -->
+    <div class="footer-bottom">
+        <p>&copy; 2025 <a href="#">LalaShoes</a>. All Rights Reserved.</p>
+    </div>
+</footer>
